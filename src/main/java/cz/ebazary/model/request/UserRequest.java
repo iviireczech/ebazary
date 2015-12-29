000e3c7b4c022478df04ee707b3c7de251efe58c
Miroslav Lacina <miroslav.lacina@gmail.com>
2015-12-29 15:16:32 +0100

    Add new attributes

--- src/main/java/cz/ebazary/model/request/UserRequest.java
+++ src/main/java/cz/ebazary/model/request/UserRequest.java
@@ -1,8 +1,18 @@
 package cz.ebazary.model.request;
 
+import cz.ebazary.model.bazaar.category.Category;
+import cz.ebazary.model.bazaar.locality.District;
+import cz.ebazary.model.bazaar.locality.Region;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 public class UserRequest {
     private String query;
+    private Category category;
+    private BigDecimal priceFrom;
+    private BigDecimal priceTo;
+    private Region region;
+    private District district;
 }
