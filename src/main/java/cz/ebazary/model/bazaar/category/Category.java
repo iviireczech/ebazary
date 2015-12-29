7cb5d6af089e2cb148abd68362bba566bf42c52a
Miroslav Lacina <miroslav.lacina@gmail.com>
2015-12-29 15:10:59 +0100

    Add Category

---
+++ src/main/java/cz/ebazary/model/bazaar/category/Category.java
@@ -0,0 +1,21 @@
+package cz.ebazary.model.bazaar.category;
+
+public enum Category {
+
+    cars,
+    motorcycles,
+    machines,
+    children,
+    home,
+    animals,
+    sport,
+    culture,
+    books,
+    fashion,
+    computers,
+    mobile_devices,
+    appliances,
+    audio_video,
+    photo
+
+}
