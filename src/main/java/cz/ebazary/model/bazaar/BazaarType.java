2b4526c954a2ee1e18e1080cc259cdc160e8a79d
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-01-08 17:36:39 +0100

    Add 'name' attribute

--- src/main/java/cz/ebazary/model/bazaar/BazaarType.java
+++ src/main/java/cz/ebazary/model/bazaar/BazaarType.java
@@ -2,6 +2,17 @@ package cz.ebazary.model.bazaar;
 
 public enum BazaarType {
 
-    sbazar, bazos, bazar
+    sbazar("sbazar.cz"),
+    bazos("bazos.cz"),
+    bazar("bazar.cz");
 
+    private String name;
+
+    BazaarType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
