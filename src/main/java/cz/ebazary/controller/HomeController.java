e2c47d559a45acc9a97ffe49468e5557a527cf03
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-01-08 17:38:12 +0100

    Add first loader implementation

--- src/main/java/cz/ebazary/controller/HomeController.java
+++ src/main/java/cz/ebazary/controller/HomeController.java
@@ -1,11 +1,17 @@
 package cz.ebazary.controller;
 
+import cz.ebazary.model.item.Item;
 import cz.ebazary.model.request.UserRequest;
+import cz.ebazary.service.item.loaders.Loadable;
+import cz.ebazary.service.item.loaders.SBazarItemLoader;
+import org.joda.time.LocalDate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -24,7 +30,10 @@ public class HomeController {
     public String result(final @ModelAttribute("userRequest") UserRequest userRequest,
                          final Model model) {
 
-        model.addAttribute(userRequest.getQuery());
+        final Loadable loadable = new SBazarItemLoader();
+        final List<Item> items = loadable.loadItems(LocalDate.now().minusDays(1));
+
+        model.addAttribute("items", items);
 
         return "result";
 
