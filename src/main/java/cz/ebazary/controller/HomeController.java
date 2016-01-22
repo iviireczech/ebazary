482e752be5172445185381a8836324bfb6ba83a2
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-01-22 23:52:44 +0100

    Add all loadables autowiring

--- src/main/java/cz/ebazary/controller/HomeController.java
+++ src/main/java/cz/ebazary/controller/HomeController.java
@@ -3,18 +3,22 @@ package cz.ebazary.controller;
 import cz.ebazary.model.item.Item;
 import cz.ebazary.model.request.UserRequest;
 import cz.ebazary.service.item.loaders.Loadable;
-import cz.ebazary.service.item.loaders.SBazarItemLoader;
 import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private List<Loadable> loadables;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(final Model model) {
@@ -30,8 +34,13 @@ public class HomeController {
     public String result(final @ModelAttribute("userRequest") UserRequest userRequest,
                          final Model model) {
 
-        final Loadable loadable = new SBazarItemLoader();
-        final List<Item> items = loadable.loadItems(LocalDate.now().minusDays(1));
+        final List<Item> items = new ArrayList<>();
+
+        loadables
+                .stream()
+                .forEach(
+                    loadable -> items.addAll(loadable.loadItems(LocalDate.now().minusDays(1)))
+                );
 
         model.addAttribute("items", items);
 
