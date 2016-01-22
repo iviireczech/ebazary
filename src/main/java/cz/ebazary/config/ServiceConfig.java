46701ead67b92548fd0b23a2dc0de646c9bbe5e9
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-01-22 23:51:58 +0100

    Add Thymeleaf JodaTimeDialect support

--- src/main/java/cz/ebazary/config/ServiceConfig.java
+++ src/main/java/cz/ebazary/config/ServiceConfig.java
@@ -3,6 +3,7 @@ package cz.ebazary.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import uk.co.gcwilliams.jodatime.thymeleaf.JodaTimeDialect;
 
 import javax.validation.Validator;
 
@@ -12,6 +13,11 @@ public class ServiceConfig {
     @Bean
     public Validator validator() {
         return new LocalValidatorFactoryBean();
+    }
+
+    @Bean
+    public JodaTimeDialect jodaTimeDialect() {
+        return new JodaTimeDialect();
     }
 
 }
