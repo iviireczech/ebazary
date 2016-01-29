f1184781967a5bda270ffccea8f8c95d57335dfd
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-01-29 17:37:08 +0100

    Remove unused attributes

--- src/main/java/cz/ebazary/model/request/UserRequest.java
+++ src/main/java/cz/ebazary/model/request/UserRequest.java
@@ -5,14 +5,10 @@ import cz.ebazary.model.bazaar.locality.District;
 import cz.ebazary.model.bazaar.locality.Region;
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
 public class UserRequest {
     private String query;
     private Category category;
-    private BigDecimal priceFrom;
-    private BigDecimal priceTo;
     private Region region;
     private District district;
 }
