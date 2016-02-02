4be33493b9232acc93df299532b0c4670dc35d26
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-02-02 22:54:19 +0100

    Add elasticsearch config

--- src/main/java/cz/ebazary/config/ServiceConfig.java
+++ src/main/java/cz/ebazary/config/ServiceConfig.java
@@ -1,14 +1,31 @@
 package cz.ebazary.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import cz.ebazary.mappers.CustomEntityMapper;
+import org.elasticsearch.client.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.EntityMapper;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import uk.co.gcwilliams.jodatime.thymeleaf.JodaTimeDialect;
 
 import javax.validation.Validator;
 
 @Configuration
 public class ServiceConfig {
+
+    @Autowired
+    private Client client;
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JodaModule());
+
+        return objectMapper;
+    }
 
     @Bean
     public Validator validator() {
@@ -16,8 +33,13 @@ public class ServiceConfig {
     }
 
     @Bean
-    public JodaTimeDialect jodaTimeDialect() {
-        return new JodaTimeDialect();
+    public EntityMapper mapper() {
+        return new CustomEntityMapper(objectMapper());
+    }
+
+    @Bean
+    public ElasticsearchTemplate elasticsearchTemplate() {
+        return new ElasticsearchTemplate(client, mapper());
     }
 
 }
