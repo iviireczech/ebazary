892a38083f337cbdc9f5fee35ad35543a1681f7f
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-02-02 22:56:17 +0100

    Add ItemService dependency

--- src/main/java/cz/ebazary/controller/HomeController.java
+++ src/main/java/cz/ebazary/controller/HomeController.java
@@ -4,17 +4,21 @@ import cz.ebazary.dto.ItemDTO;
 import cz.ebazary.model.bazaar.category.Category;
 import cz.ebazary.model.bazaar.locality.Region;
 import cz.ebazary.model.request.UserRequest;
+import cz.ebazary.service.item.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private ItemService itemService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(final Model model) {
@@ -32,7 +36,14 @@ public class HomeController {
     public String result(final @ModelAttribute("userRequest") UserRequest userRequest,
                          final Model model) {
 
-        final List<ItemDTO> itemDTOs = new ArrayList<>();
+        final List<ItemDTO> itemDTOs =
+                itemService
+                        .getItems(
+                                null,
+                                userRequest.getCategory(),
+                                userRequest.getQuery()
+                        );
+
         model.addAttribute("items", itemDTOs);
 
         return "result";
