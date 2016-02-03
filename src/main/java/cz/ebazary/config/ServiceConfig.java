02e103de466400e073c8f59dab7660b3b5dbfb53
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-02-03 11:13:06 +0100

    Remove unused configuration

--- src/main/java/cz/ebazary/config/ServiceConfig.java
+++ src/main/java/cz/ebazary/config/ServiceConfig.java
@@ -1,14 +1,7 @@
 package cz.ebazary.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-import cz.ebazary.mappers.CustomEntityMapper;
-import org.elasticsearch.client.Client;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.EntityMapper;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.validation.Validator;
@@ -16,30 +9,9 @@ import javax.validation.Validator;
 @Configuration
 public class ServiceConfig {
 
-    @Autowired
-    private Client client;
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        final ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JodaModule());
-
-        return objectMapper;
-    }
-
     @Bean
     public Validator validator() {
         return new LocalValidatorFactoryBean();
-    }
-
-    @Bean
-    public EntityMapper mapper() {
-        return new CustomEntityMapper(objectMapper());
-    }
-
-    @Bean
-    public ElasticsearchTemplate elasticsearchTemplate() {
-        return new ElasticsearchTemplate(client, mapper());
     }
 
 }
