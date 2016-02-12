eb98102edca44816e1dce95bfd108a80fdc827b2
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-02-12 16:45:28 +0100

    Remove unused test

--- src/test/java/cz/ebazary/scheduling/ItemLoaderJobTest.java
+++ src/test/java/cz/ebazary/scheduling/ItemLoaderJobTest.java
@@ -1,7 +1,6 @@
-package cz.ebazary.service.item.loaders;
+package cz.ebazary.scheduling;
 
 import cz.ebazary.Application;
-import org.joda.time.LocalDate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +9,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
-public class HyperinzerceItemLoaderTest {
+public class ItemLoaderJobTest {
 
     @Autowired
-    private Loadable hyperinzerceItemLoader;
+    private ItemLoaderJob itemLoaderJob;
 
     @Test
-    public void testLoadItems() throws Exception {
-
-        hyperinzerceItemLoader.loadItems(LocalDate.now().minusDays(1));
-
+    public void testLoadNewItems() throws Exception {
+        itemLoaderJob.loadNewItems();
     }
-
 }
